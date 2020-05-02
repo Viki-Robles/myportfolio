13b0c0e5fd038a7fc61d69f9d55b3572f4750214
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-05-02 23:49:04 +0100

    styling the container

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -4,13 +4,12 @@ import './HomePage.css';
 export default function HomePage() {
     return(
         <div className="homePage">
-            <h6>"body"</h6>
+            <div className="bracket">&#123;</div>
             <h1>Hi, <br/>I am Vicky</h1>
-            <h2>your web developer &#9813;</h2>
-            <h4>Front End Developer</h4>
+            <h2>your web developer &#9813;.</h2>
+            <h4> Front End Developer</h4>
             <button>Contact me</button>
-            <h6>"/body"</h6>
-
+            <div className="bracket bracket-down">&#125;</div>
         </div>
     );
 }
