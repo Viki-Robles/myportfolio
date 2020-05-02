80d999fcb6ad42e4876eaeb4d29bd4c7d7ac7745
Vicky <vicky@Vickys-MacBook-Pro.local>
2020-05-03 00:35:02 +0100

    adding logo onthecontainer page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,15 +1,20 @@
 import React from "react";
-import './HomePage.css';
+import "./HomePage.css";
 
 export default function HomePage() {
-    return(
-        <div className="homePage">
-            <div className="bracket">&#123;</div>
-            <h1>Hi, <br/>I am Vicky</h1>
-            <h2>your web developer &#9813;.</h2>
-            <h4> Front End Developer</h4>
-            <button>Contact me</button>
-            <div className="bracket bracket-down">&#125;</div>
-        </div>
-    );
+  return (
+    <div className="homePage-container">
+    <div className="homePage-logo">&#120121;</div>
+      <div className="homePage">
+        <div className="bracket">&#123;</div>
+        <h1>
+          Hi, <br />I am Vicky
+        </h1>
+        <h2>your web developer &#9813;.</h2>
+        <h4> Front End Developer</h4>
+        <button>Contact me</button>
+        <div className="bracket bracket-down">&#125;</div>
+      </div>
+    </div>
+  );
 }
